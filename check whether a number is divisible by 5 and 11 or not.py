79eddd6e4960785cd48cf95a27259b30d97d508c
mehreenkhursheed <mehreenkhursheed12@gmail.com>
2024-10-21 09:08:18 +0530

    Add files via upload

---
+++ check whether a number is divisible by 5 and 11 or not.py
@@ -0,0 +1,9 @@
+number=int(input("any number:"))
+if number % 5==0 and number % 11==0:
+    print("number is divisible by 5 but not by 11")
+elif number % 5==0 and number % 11==0:
+    print("number is divisible by both 5 and 11")
+elif number % 5==0 and number % 11==0:
+    print("number is divisible by 11 but not by 5")
+else:
+    print("neither divisible by 5 nor by 11")
